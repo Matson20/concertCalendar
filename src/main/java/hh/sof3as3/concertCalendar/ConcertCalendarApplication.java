2f51615f2948972a4d62f3781e71a8d0f5fcb7ab
Matias Luoto <luotomatias@gmail.com>
2023-03-28 15:57:51 +0300

    first commit

---
+++ src/main/java/hh/sof3as3/concertCalendar/ConcertCalendarApplication.java
@@ -0,0 +1,13 @@
+package hh.sof3as3.concertCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/sof3as3/concertCalendar/ConcertCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof3as3.concertCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
